9ca8f3068d10352e0a9e59724626883ce9268b6b
Chuks <chuskmbanasoj@gmail.com>
2022-01-13 07:30:44 +0100

    Updated files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,13 +17,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-import debug_toolbar
 
 
 urlpatterns = [
     path('', include('port.urls')),
     path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
